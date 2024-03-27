52cb9a9c5dc0936700458e1d16a5442418a3c4bb
stachoz <stasiekzdybel9@gmail.com>
2024-03-28 00:15:46 +0100

    modified response from AuthenticationEntryPoint

--- src/main/java/com/example/cryptoapp/security/JWTAuthEntryPoint.java
+++ src/main/java/com/example/cryptoapp/security/JWTAuthEntryPoint.java
@@ -11,8 +11,11 @@ import java.io.IOException;
 
 @Component
 public class JWTAuthEntryPoint implements AuthenticationEntryPoint {
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getOutputStream().println("{ \"error\": \"" + "JWT was expired or incorrect" + "\" }");
     }
 }
